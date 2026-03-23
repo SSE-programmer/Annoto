6203c8e0b25cec90a84d0f0b5f95739b0298d948
Stanislav Stolbennikov <stolbennikovilmk@gmail.com>
2026-03-24 01:27:20 +0700

    Added color theme service

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,19 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import {
+    ApplicationConfig, inject,
+    provideAppInitializer,
+    provideBrowserGlobalErrorListeners,
+    provideZonelessChangeDetection
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { ThemeService } from '@shared/services/theme.service';
 
 export const appConfig: ApplicationConfig = {
     providers: [
         provideBrowserGlobalErrorListeners(),
         provideRouter(routes),
-        provideZonelessChangeDetection()
+        provideZonelessChangeDetection(),
+        provideAppInitializer(() => void inject(ThemeService)),
     ]
 };
---
+++ src/app/shared/services/theme.service.ts
@@ -0,0 +1,56 @@
+import { effect, Injectable, signal } from '@angular/core';
+
+export enum EThemeType {
+    LIGHT = 'light',
+    DARK = 'dark',
+}
+
+const DEFAULT_THEME = EThemeType.LIGHT;
+const STORAGE_THEME_FIELD_NAME = 'theme:color';
+const THEME_CHANGE_TRANSITION_CLASS = 'global-color-transition'
+const THEME_PREFIX = 'an-theme-';
+
+@Injectable({ providedIn: 'root' })
+export class ThemeService {
+    private readonly _colorThemeSignal = signal<EThemeType>(this._loadInitialThemeState());
+
+    private readonly _colorThemeChangeEffect = effect(() => {
+        const theme = this._colorThemeSignal();
+
+        localStorage.setItem(STORAGE_THEME_FIELD_NAME, theme);
+        this._applyThemeToBody(theme);
+    });
+
+    public toggleColorTheme(theme: EThemeType): void {
+        this._colorThemeSignal.update(() => theme);
+    }
+
+    private _loadInitialThemeState() {
+        const saved = localStorage.getItem(STORAGE_THEME_FIELD_NAME);
+        const foundedTheme = Object
+            .values(EThemeType)
+            .find((theme: EThemeType) => theme === saved);
+
+        if (foundedTheme) {
+            return foundedTheme;
+        }
+
+        return window.matchMedia?.('(prefers-color-scheme: dark)').matches ? EThemeType.DARK : DEFAULT_THEME;
+    }
+
+    private _applyThemeToBody(theme: EThemeType): void {
+        const classList = document.body.classList;
+
+        Array.from(classList)
+            .filter(className => className.startsWith(THEME_PREFIX))
+            .forEach(className => classList.remove(className));
+
+        classList.add(`${ THEME_PREFIX }${ theme }`);
+        classList.add(THEME_CHANGE_TRANSITION_CLASS);
+        localStorage.setItem(STORAGE_THEME_FIELD_NAME, theme.toString());
+
+        setTimeout(() => {
+            classList.remove(THEME_CHANGE_TRANSITION_CLASS);
+        }, 500);
+    }
+}
