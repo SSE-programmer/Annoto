eb4f455bb1302121c8b812ab91e52a81c53a68e6
Stanislav Stolbennikov <stolbennikovilmk@gmail.com>
2026-03-23 23:50:05 +0700

    Base project settings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,10 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
+  selector: 'an-app-root',
   imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
 })
-export class App {
-  protected readonly title = signal('Annoto');
+export class AppComponent {
 }
