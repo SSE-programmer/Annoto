f23c3e537b36e2172cbaf62120911cbbbd8de894
Stanislav Stolbennikov <stolbennikovilmk@gmail.com>
2026-03-26 18:48:12 +0700

    Added view transition for routing

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,7 +5,7 @@ import {
     provideBrowserGlobalErrorListeners,
     provideZonelessChangeDetection
 } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, Router, withViewTransitions } from '@angular/router';
 
 import { routes } from './app.routes';
 import { ThemeService } from '@shared/services/theme.service';
@@ -13,7 +13,25 @@ import { ThemeService } from '@shared/services/theme.service';
 export const appConfig: ApplicationConfig = {
     providers: [
         provideBrowserGlobalErrorListeners(),
-        provideRouter(routes),
+        provideRouter(
+            routes,
+            withViewTransitions({
+                onViewTransitionCreated: ({transition}) => {
+                    const router = inject(Router);
+                    const targetUrl = router.getCurrentNavigation()?.finalUrl || '';
+                    const config = {
+                        paths: 'exact',
+                        matrixParams: 'exact',
+                        fragment: 'ignored',
+                        queryParams: 'ignored',
+                    } as const;
+
+                    if (router.isActive(targetUrl, config)) {
+                        transition.skipTransition();
+                    }
+                },
+            })
+        ),
         provideZonelessChangeDetection(),
         provideAppInitializer(() => void inject(ThemeService)),
     ]
